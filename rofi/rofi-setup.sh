796b5f888dbf48185de18470f03559a697ad817c
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-14 00:25:51 +0100

    Add Rofi installation and config setup script
    This script installs Rofi and imports the configuration file from a remote repository.

---
+++ rofi/rofi-setup.sh
@@ -0,0 +1,4 @@
+# Install Rofi
+sudo nala install rofi
+# Import config
+curl -sL https://raw.githubusercontent.com/itsPoipoi/linux-setup/refs/heads/main/rofi/config.rasi --create-dirs -o ~/.config/rofi/config.rasi
