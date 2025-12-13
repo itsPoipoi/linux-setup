84f2448818f44fd8409255e23615c3373cf80fb2
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-14 00:37:49 +0100

    Enhance setup script with additional installations
    Updated setup script to include installation of various tools and configuration files.

--- setup.sh
+++ setup.sh
@@ -3,12 +3,15 @@ GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 YELLOW=$'\e[0;33m'
 NC=$'\e[0m'
+
 # Install Nala for all following installs
 sudo apt-get install nala -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo nala upgrade -y
+
 # Install zsh in case it's not already installed
 sudo nala install zsh -y
+
 # Change default shell to zsh
 if [ ! "{{$SHELL}}" = "{{/usr/bin/zsh}}" ]; then
   echo "${YELLOW}Change default shell to zsh ?${NC} "
@@ -19,11 +22,14 @@ if [ ! "{{$SHELL}}" = "{{/usr/bin/zsh}}" ]; then
 	  esac
   done
 fi
+
 # Import .zshrc
 curl -sL https://raw.githubusercontent.com/itsPoipoi/linux-setup/main/.zshrc -o ~/.zshrc
 echo "${YELLOW}.zshrc imported.${NC}"
+
 # Install git, trash, neovim, batcat, unzip, fontconfig, etc
 sudo nala install git trash-cli neovim bat unzip fontconfig make gcc ripgrep xclip -y
+
 # Install Nerd Font
 if [ ! -f ~/.local/share/fonts/FiraCodeNerdFont-Medium.ttf ]; then
 	echo "${GREEN}Installing FiraCode Nerd Font...${NC}"
@@ -35,6 +41,7 @@ if [ ! -f ~/.local/share/fonts/FiraCodeNerdFont-Medium.ttf ]; then
 	echo "${RED}Font installed. Remember to select FiraCode font!${NC}"
  	else echo "${GREEN}FiraCode Nerd Font is already installed. Skipping.${NC}"
  fi
+ 
 # Install Homebrew & dependencies
 if [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
   echo "${YELLOW}Installing Homebrew...${NC}"
@@ -42,21 +49,25 @@ if [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
   else echo "${YELLOW}Homebrew is already installed. Skipping.${NC}"
 fi
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 # Install fzf
 if [ ! -f /home/linuxbrew/.linuxbrew/bin/fzf ]; then
   brew install -q fzf
   else echo "${GREEN}Fzf is already installed. Skipping.${NC}"
 fi
+
 # Install eza
 if [ ! -f /home/linuxbrew/.linuxbrew/bin/eza ]; then
   brew install -q eza
   else echo "${YELLOW}Eza is already installed. Skipping.${NC}"
 fi
+
 # Install zoxide
 if [ ! -f ~/.local/bin/zoxide ]; then
   /bin/bash -c "$(curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh)"
   else echo "${GREEN}Zoxide is already installed. Skipping.${NC}"
 fi
+
 # Install Fastfetch
 if [ ! -f /usr/bin/fastfetch ]; then
   FASTFETCH_URL=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest | grep "browser_download_url.*linux-amd64.deb" | cut -d '"' -f 4)
@@ -64,6 +75,7 @@ if [ ! -f /usr/bin/fastfetch ]; then
   sudo nala install /tmp/fastfetch_latest_amd64.deb -y
   else sudo nala install fastfetch -y
 fi
+
 # Import Fastfetch config
 if [ ! -f ~/.config/fastfetch/config.jsonc ]; then
 	fastfetch --gen-config ~/.config/fastfetch/config.jsonc
@@ -77,5 +89,6 @@ else
 		esac
  	done
 fi
+
 # Complete message
 echo "${GREEN}Setup complete! Profile reloading!${NC}"; zsh
