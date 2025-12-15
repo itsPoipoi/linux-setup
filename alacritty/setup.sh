8d44f63af21a09b5b0f75b86069d08eee7af2348
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-16 00:10:00 +0100

    Add setup script for Alacritty installation
    Install Alacritty and import its configuration file.

---
+++ alacritty/setup.sh
@@ -0,0 +1,4 @@
+# Install Rofi
+sudo nala install alacritty -y
+# Import config
+curl -sL https://raw.githubusercontent.com/itsPoipoi/linux-setup/refs/heads/main/alacritty/alacritty.toml --create-dirs -o ~/.config/alacritty/alacritty.toml
