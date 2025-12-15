9543f0cf7588ae5bc8fec7e0487e23dc91b5a002
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-16 00:50:12 +0100

    Update setup script to install dependencies and Alacritty

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,4 +1,8 @@
-# Install Rofi
-sudo nala install alacritty -y
+# Install Deps
+sudo nala install cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+# Install Cargo
+curl https://sh.rustup.rs -sSf | sh
+# Install Alacritty
+cargo install alacritty
 # Import config
 curl -sL https://raw.githubusercontent.com/itsPoipoi/linux-setup/refs/heads/main/alacritty/alacritty.toml --create-dirs -o ~/.config/alacritty/alacritty.toml
