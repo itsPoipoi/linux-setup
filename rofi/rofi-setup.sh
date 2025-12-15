e34a58a41a446b224277b11de4351701c68c9d46
itsPoipoi <61839795+itsPoipoi@users.noreply.github.com>
2025-12-16 00:08:59 +0100

    Add '-y' flag to Rofi installation command

--- rofi/rofi-setup.sh
+++ rofi/rofi-setup.sh
@@ -1,4 +1,4 @@
 # Install Rofi
-sudo nala install rofi
+sudo nala install rofi -y
 # Import config
 curl -sL https://raw.githubusercontent.com/itsPoipoi/linux-setup/refs/heads/main/rofi/config.rasi --create-dirs -o ~/.config/rofi/config.rasi
